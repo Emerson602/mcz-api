920a2cb93b8bf68eb4b766b9329cd3859111e25c
Emerson602 <emerson.nic@hotmail.com>
2025-07-19 01:18:02 -0300

    update app.js

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 
 app.use('/customers', customerRoutes);
